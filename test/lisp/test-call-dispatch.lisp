5b76bf851f153a4224d03d63f2e1a7a26d3eea37
tamura shingo <tamura.shingo@gmail.com>
2026-07-18 02:36:25 +0000

    milestone 95 - 目的   - Lisp-2化(documents/lisp2_conversion.md)の仕上げ。milestone94で一時的に許容していた     test-call-dispatch.lispの失敗を解消し、既存コード全体をLisp-2の前提に合わせて監査する - 対応内容   - test/lisp/test-call-dispatch.lisp(milestone61)を全面書き換えし、bareで渡していた     car/+/</cd-double/cd-add/cd-ltの全箇所へ#'を付与した(funcall/apply/reduce/sortが     呼び出し先の種別を問わず一貫動作することを検証する、というテストの意図自体は不変)   - milestone93〜94着手前のgrep(mapcar/reduce/sort/apply/funcallの引数)でカバーされない     他パターン(defvar/setq/letでグローバル関数名をbareで保持・受け渡し)を新たにgrepで     広く再監査したが、lisp/*.lisp・test/lisp/*.lisp全体で新規の破壊箇所は見つからなかった     (94のコミット時点で既に本命の箇所は洗い出し済みだったことを確認する形になった)   - documents/lisp2_conversion.mdのマイルストーン95の状態列を完了に更新した   - README.mdにdocuments/lisp2_conversion.mdへのリンクを追加し、ドキュメント数を11に更新した - 確認内容   - make buildが通ることを確認した   - make test(全25フィクスチャ)を実行し、書き換え後のtest-call-dispatchを含む全ファイルが     PASSすることを確認した(=== ALL PASS ===)。新しいpanicシナリオは導入していないため、     milestone90と同様個別QEMU対話での追加確認は不要と判断した

--- test/lisp/test-call-dispatch.lisp
+++ test/lisp/test-call-dispatch.lisp
@@ -7,6 +7,9 @@
 ; 種別(Cビルトイン・コンパイル済みクロージャ(milestone60でdefunの既定)・
 ; インタプリタクロージャ(lambda式の値、milestone58以降もコンパイルされない))
 ; いずれに対しても一貫して動作することを確認する。
+;
+; milestone94(Lisp-2化)により、グローバル関数名をbareで値として渡すとunbound variableに
+; なるため、以下すべての箇所で#'を付けて関数セルの値を渡すように書き換えている。
 
 ; equal-ints: test-stdlib.lispで既に定義済みの構造比較ヘルパーと同じ実装
 (defun cd-equal-ints (a b)
@@ -21,24 +24,24 @@
 ; funcall: builtin・コンパイル済みクロージャ(defun)・インタプリタクロージャ(lambda)の
 ; 3種いずれもfnとして渡して一貫して呼び出せることを確認する
 (defun run-test-call-dispatch-funcall ()
-  (and (eq (funcall car (list 1 2 3)) 1)
-       (eq (funcall cd-double 5) 10)
+  (and (eq (funcall #'car (list 1 2 3)) 1)
+       (eq (funcall #'cd-double 5) 10)
        (eq (funcall (lambda (x) (+ x 100)) 5) 105)))
 
 ; apply: 引数は評価済みの実引数「リスト」として渡す点がfuncallと異なる
 (defun run-test-call-dispatch-apply ()
-  (and (eq (apply car (list (list 1 2 3))) 1)
-       (eq (apply cd-double (list 5)) 10)
+  (and (eq (apply #'car (list (list 1 2 3))) 1)
+       (eq (apply #'cd-double (list 5)) 10)
        (eq (apply (lambda (x) (+ x 100)) (list 5)) 105)
-       (eq (apply + (list 1 2)) 3)))
+       (eq (apply #'+ (list 1 2)) 3)))
 
 ; reduce(内部でfuncallを使う): builtin(+)・コンパイル済み(cd-double相当の畳み込み用
 ; 関数)・lambdaいずれをfnとして渡しても一貫した結果になることを確認する
 (defun cd-add (a b) (+ a b))
 
 (defun run-test-call-dispatch-reduce ()
-  (and (eq (reduce + (list 1 2 3 4) 0) 10)
-       (eq (reduce cd-add (list 1 2 3 4) 0) 10)
+  (and (eq (reduce #'+ (list 1 2 3 4) 0) 10)
+       (eq (reduce #'cd-add (list 1 2 3 4) 0) 10)
        (eq (reduce (lambda (acc x) (+ acc x)) (list 1 2 3 4) 0) 10)))
 
 ; sort(内部でapplyを使う): builtin(<)・コンパイル済み・lambdaいずれをpredとして
@@ -46,10 +49,10 @@
 (defun cd-lt (a b) (< a b))
 
 (defun run-test-call-dispatch-sort ()
-  (and (cd-equal-ints (sort < (list 3 1 4 1 5 9 2 6)) (list 1 1 2 3 4 5 6 9))
-       (cd-equal-ints (sort cd-lt (list 3 1 2)) (list 1 2 3))
+  (and (cd-equal-ints (sort #'< (list 3 1 4 1 5 9 2 6)) (list 1 1 2 3 4 5 6 9))
+       (cd-equal-ints (sort #'cd-lt (list 3 1 2)) (list 1 2 3))
        (cd-equal-ints (sort (lambda (a b) (< a b)) (list 3 1 2)) (list 1 2 3))
-       (null (sort < nil))))
+       (null (sort #'< nil))))
 
 (defun run-test-call-dispatch ()
   (and (run-test-call-dispatch-funcall)
